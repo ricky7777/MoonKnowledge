a6ce353f607344f5a76c38c703388170a2002145
Ricky <ricky nt86307@gmail.com>
2022-07-05 16:04:49 +0800

    [Init]init first version

---
+++ app/src/main/java/com/cathaybk/dbs/beanknowledge/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.cathaybk.dbs.beanknowledge
+
+import android.app.Activity
+import android.os.Bundle
+
+/**
+ * Created by Ricky on 2022/7/5.
+ */
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+    }
+}
