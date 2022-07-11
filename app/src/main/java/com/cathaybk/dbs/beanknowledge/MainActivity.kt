66550725b2e9eb9756757e30f1acd851b456c4e3
gdb-bulga <bulga0202@gmail.com>
2022-07-11 15:30:06 +0800

    [Modify] the num can add ver.0711-3

--- app/src/main/java/com/cathaybk/dbs/beanknowledge/MainActivity.kt
+++ app/src/main/java/com/cathaybk/dbs/beanknowledge/MainActivity.kt
@@ -2,13 +2,44 @@ package com.cathaybk.dbs.beanknowledge
 
 import android.app.Activity
 import android.os.Bundle
+import com.cathaybk.dbs.beanknowledge.databinding.MainActivityBinding
 
 /**
  * Created by Ricky on 2022/7/5.
  */
 class MainActivity : Activity() {
+    lateinit var binding: MainActivityBinding
+    var rightNumber = 0
+    var leftNumber = 0
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+        binding = MainActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.tvWaitingNumLeft.text = (leftNumber).toString()
+        binding.tvWaitingNumRight.text = (rightNumber).toString()
+
+        binding.tvCancelTrading.setOnClickListener{ cancelTrading() }
+    }
+
+
+    private fun cancelTrading() {
+
+        if(rightNumber == 9) {
+            if(leftNumber == 9) return
+
+            rightNumber = 0
+            leftNumber += 1
+            binding.tvWaitingNumLeft.text = (leftNumber).toString()
+            binding.tvWaitingNumRight.text = (rightNumber).toString()
+        }
+        else{
+            rightNumber += 1
+            binding.tvWaitingNumRight.text = (rightNumber).toString()
+        }
+
+
     }
 }
