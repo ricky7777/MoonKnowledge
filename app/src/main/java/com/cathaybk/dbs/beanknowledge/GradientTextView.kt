18e1037df86f0bead0e7a99f228fcc4c3540fc45
gdb-bulga <44048482+gdb-bulga@users.noreply.github.com>
2022-07-13 09:44:59 +0800

    [Modify] UI : the num can add (just for button test) and the num's color is done  (#3)
    * [Modify] test
    
    * [HouYi] app with an image2
    
    * [Modify] the num can add ver.0711-3
    
    * [Modify] the num can add and its color is done ver.0711-4
    
    * [Modify UpperLeft Group] ConstraintLayout -> LinearLayout
    
    * [Modify Upper/BottomLeft Group] ConstraintLayout -> LinearLayout
    
    * [Modify] Group some code in MainActivity.kt
    
    * [Modify] Group some code in MainActivity.kt with apply
    
    * [Modify] Group some code in MainActivity.kt with apply2
    
    * [Modify] private and updateVariable

---
+++ app/src/main/java/com/cathaybk/dbs/beanknowledge/GradientTextView.kt
@@ -0,0 +1,44 @@
+package com.cathaybk.dbs.beanknowledge
+
+import android.content.Context
+import android.graphics.LinearGradient
+import android.graphics.Shader
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.core.content.ContextCompat
+
+/**
+ * @author Tom on 2022/1/17
+ * TextView with gradient color
+ */
+class GradientTextView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) :
+    AppCompatTextView(context, attrs, defStyleAttr) {
+    private var colorStart = ContextCompat.getColor(context, R.color.black)
+    private var colorEnd = ContextCompat.getColor(context, R.color.white)
+
+    init {
+        context.theme.obtainStyledAttributes(attrs, R.styleable.GradientTextView, 0, 0).apply {
+            colorStart = getColor(R.styleable.GradientTextView_startTextColor, colorStart)
+            colorEnd = getColor(R.styleable.GradientTextView_endTextColor, colorEnd)
+        }.recycle()
+    }
+
+    override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
+        super.onLayout(changed, left, top, right, bottom)
+        //Setting the gradient if layout is changed
+        if (changed) {
+            paint.shader = getLinearGradient()
+        }
+    }
+
+    fun setGradientColors(colorStart: Int, colorEnd: Int) {
+        this.colorStart = colorStart
+        this.colorEnd = colorEnd
+        // this forces view redrawing
+        invalidate()
+    }
+
+    private fun getLinearGradient() = LinearGradient(
+        0f, 0f, width.toFloat(), 0f, colorStart, colorEnd, Shader.TileMode.CLAMP
+    )
+}
--- app/src/main/java/com/cathaybk/dbs/beanknowledge/MainActivity.kt
+++ app/src/main/java/com/cathaybk/dbs/beanknowledge/MainActivity.kt
@@ -2,13 +2,59 @@ package com.cathaybk.dbs.beanknowledge
 
 import android.app.Activity
 import android.os.Bundle
+import com.cathaybk.dbs.beanknowledge.databinding.MainActivityBinding
+import com.cathaybk.dbs.beanknowledge.databinding.UpperLeftContentBinding
 
 /**
  * Created by Ricky on 2022/7/5.
  */
 class MainActivity : Activity() {
+    private lateinit var binding: MainActivityBinding
+    private var rightNumber = 0
+    private var leftNumber = 0
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+        binding = MainActivityBinding.inflate(layoutInflater)
+            .apply {
+                setContentView(root)
+                upperLeftContent.tvCancelTrading.setOnClickListener { cancelTrading() }
+            }
+        updateVariable()
+    }
+
+
+    private fun cancelTrading() {
+
+        if (rightNumber == 9 && leftNumber == 9) {
+            return
+        }
+
+        if (rightNumber == 9) {
+            calculCarryNum()
+        } else {
+            calculNum()
+        }
+
+        updateVariable()
+
+    }
+
+    private fun updateVariable() {
+        binding.upperLeftContent.apply {
+            tvWaitingNumLeft.text = (leftNumber).toString()
+            tvWaitingNumRight.text = (rightNumber).toString()
+
+        }
+    }
+
+    private fun calculCarryNum() {
+        rightNumber = 0
+        leftNumber += 1
+    }
+
+    private fun calculNum() {
+        rightNumber += 1
     }
 }
