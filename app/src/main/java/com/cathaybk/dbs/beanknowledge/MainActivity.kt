fa0a7fe7b6e41ab245ceb6533574c6c0112a9f8b
gdb-bulga <bulga0202@gmail.com>
2022-07-18 11:40:35 +0800

    [Modify] UpperLeft UI: change statusBarColor to transparent '

--- app/src/main/java/com/cathaybk/dbs/beanknowledge/MainActivity.kt
+++ app/src/main/java/com/cathaybk/dbs/beanknowledge/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.cathaybk.dbs.beanknowledge
 
 import android.app.Activity
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+import android.view.WindowManager
+import android.view.WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS
 import com.cathaybk.dbs.beanknowledge.databinding.MainActivityBinding
-import com.cathaybk.dbs.beanknowledge.databinding.UpperLeftContentBinding
+
 
 /**
  * Created by Ricky on 2022/7/5.
@@ -15,6 +21,7 @@ class MainActivity : Activity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setStatusBarTransparent()
         super.onCreate(savedInstanceState)
         binding = MainActivityBinding.inflate(layoutInflater)
             .apply {
@@ -23,7 +30,6 @@ class MainActivity : Activity() {
             }
         updateVariable()
     }
-
 
     private fun cancelTrading() {
 
@@ -57,4 +63,21 @@ class MainActivity : Activity() {
     private fun calculNum() {
         rightNumber += 1
     }
+
+    private fun setStatusBarTransparent() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            //5.0 全透明实现
+            val window = window
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            window.decorView.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR or View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            window.statusBarColor = Color.TRANSPARENT
+            window.navigationBarColor = Color.rgb(242,242,242)
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            //4.4 全透明状态栏
+            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        }
+    }
+
 }
